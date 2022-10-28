0e1442279b521f8278dd556d8efd2d11d2faf672
Batuhan Okur <batuhanokur2209@gmail.com>
2022-10-28 17:15:36 +0300

    operatingSystems field added like string array and its getter and setter methods have been added

--- src/com/luv2code/springdemo/mvc/Student.java
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -12,6 +12,8 @@ public class Student {
 	
 	private String favoriteLanguage;
 	
+	private String[] operatingSystems;
+	
 	public Student() {
 		// populate country options: used ISO country code
 		countryOptions = new LinkedHashMap<>();
@@ -57,6 +59,16 @@ public class Student {
 	public void setFavoriteLanguage(String favoriteLanguage) {
 		this.favoriteLanguage = favoriteLanguage;
 	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
+
+	
 	
 	
 }
